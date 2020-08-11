fcf1541ea4f5f20b261337062ebe4ebf103afe7e
b2stts2b <axelthunell1337@gmail.com>
2020-08-11 23:37:54 +0200

    Revamped game1
    Now the grid bars increase how much the cps should be multiplied with.

--- script/game1.js
+++ script/game1.js
@@ -1,80 +1,56 @@
-let money = 0;
-let moneyHeight = 10;
-let candy = 0;
-let candyHeight = 10;
-let books = 0;
-let booksHeight = 10;
+let currencyText = document.getElementById("currency-text");
+let bars = document.getElementsByClassName("col");
+let barMovers = document.getElementsByClassName('bar-mover');
+let barTexts = document.getElementsByClassName("bar-text");
+let hover = new Array(bars.length).fill(false);
+let barHeights = new Array(barMovers.length).fill(0);
+
+
+let currency = 0;
+let currencyPerSec = 1;
 let FRAMERATE = 30;
+let maxHeight = bars[0].clientHeight;
 
-let moneyText = document.getElementById("money");
-let candyText = document.getElementById("candy");
-let booksText = document.getElementById("books");
+//Add mouseover event
+for (let i = 0; i < bars.length; i++) {
+	bars[i].addEventListener("mouseover",function(){
+		hover[i] = true;
+	})
+}
+for (let i = 0; i < bars.length; i++) {
+	bars[i].addEventListener("mouseout",function(){
+		hover[i] = false;
+	})
+}
 
-let moneyBar = document.getElementById('money-bar');
-let moneyBarMover = document.getElementById("money-bar-mover");
-let candyBar = document.getElementById('candy-bar');
-let candyBarMover = document.getElementById("candy-bar-mover");
-let booksBar = document.getElementById('books-bar');
-let booksBarMover = document.getElementById("books-bar-mover");
-
-let hoverMoney = false;
-let hoverCandy = false;
-let hoverBooks = false;
-
-let maxHeight = moneyBar.clientHeight;
 setInterval(updateHeight, 1000/FRAMERATE);
-
-moneyBar.addEventListener("mouseover", function(){
-	hoverMoney = true;
-})
-
-moneyBar.addEventListener("mouseout", function(){
-	hoverMoney = false;
-})
-
-candyBar.addEventListener("mouseover", function(){
-	hoverCandy = true;
-})
-
-candyBar.addEventListener("mouseout", function(){
-	hoverCandy = false;
-})
-
-booksBar.addEventListener("mouseover", function(){
-	hoverBooks = true;
-})
-
-booksBar.addEventListener("mouseout", function(){
-	hoverBooks = false;
-})
+setInterval(updateCurrency, 1000/FRAMERATE);		
+setInterval(updateText, 1000/FRAMERATE);
 
 function updateHeight(){
-	if (hoverMoney) {
-		moneyHeight = Math.min(moneyHeight+3, maxHeight);
-	} else{
-		moneyHeight = Math.max(moneyHeight-1, 10);
+	for (let i = 0; i < bars.length; i++) {
+		if (hover[i]){
+			barHeights[i] = Math.min(barHeights[i]+1, maxHeight);
+			barMovers[i].style.height = `${barHeights[i]}px`
+		} else {
+			barHeights[i] = Math.max(barHeights[i]-1, 0);
+			barMovers[i].style.height = `${barHeights[i]}px`;
+		}
 	}
-	if (hoverCandy) {
-		candyHeight = Math.min(candyHeight+3, maxHeight);
-	} else{
-		candyHeight = Math.max(candyHeight-1, 10);
+}
+
+function updateCurrency(){
+	let newCurrencyPerSec = 1;
+	for (let j = 0; j < barMovers.length; j++) {
+		newCurrencyPerSec *= 1+Math.floor(barHeights[j]/10);
 	}
-	if (hoverBooks) {
-		booksHeight = Math.min(booksHeight+3, maxHeight);
-	} else{
-		booksHeight = Math.max(booksHeight-1, 10);
+	currencyPerSec = newCurrencyPerSec;
+	currency += currencyPerSec/FRAMERATE;
+}
+
+function updateText(){
+	currencyText.innerHTML = `Currency: ${Math.floor(currency)} (${Math.floor(currencyPerSec)}/s)`
+	for (let k = 0; k < barTexts.length; k++) {
+		barTexts[k].innerHTML = `x${1+Math.floor(barHeights[k]/10)}`;
 	}
-
-	moneyBarMover.style.height = `${moneyHeight}px`;
-	candyBarMover.style.height = `${candyHeight}px`;
-	booksBarMover.style.height = `${booksHeight}px`;
-
-	money += Math.floor(moneyHeight/10);
-	candy += Math.floor(candyHeight/10);
-	books += Math.floor(booksHeight/10);
-
-	moneyText.innerHTML = `Money: ${money}`;
-	candyText.innerHTML = `Candy: ${candy}`;
-	booksText.innerHTML = `Books: ${books}`;
-
 }
